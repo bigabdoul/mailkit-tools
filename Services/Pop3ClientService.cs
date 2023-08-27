f57335b65b339c3fab9495c81fe10b3a2c672ffe
bigabdoul <kababdoul@hotmail.com>
2023-08-27 15:31:18 +0000

    minor editing

--- Services/Pop3ClientService.cs
+++ Services/Pop3ClientService.cs
@@ -13,8 +13,8 @@ namespace MailkitTools.Services
     public class Pop3ClientService : EmailClientService, IPop3ClientService
     {
         /// <summary>
-        /// If true, uses the base implementation of <see cref="CreateIncomingMailClientAsync(CancellationToken, RemoteCertificateValidationCallback)"/>, which uses the <see cref="ImapClient"/> by default.
-        /// Otherwise, uses the <see cref="Pop3Client"/>.
+        /// If true, uses the base implementation of <see cref="CreateIncomingMailClientAsync(CancellationToken, RemoteCertificateValidationCallback)"/>, 
+        /// which uses the <see cref="ImapClient"/> by default. Otherwise, uses the <see cref="Pop3Client"/>.
         /// </summary>
         public bool UseImapClient { get; set; }
 
@@ -31,9 +31,9 @@ namespace MailkitTools.Services
         /// <param name="cancellationToken">The token used to cancel an ongoing async operation.</param>
         /// <param name="certificateValidator">A callback function to validate the server certificate.</param>
         /// <returns></returns>
-        protected override async Task<IMailService> CreateIncomingMailClientAsync(CancellationToken cancellationToken = default(CancellationToken), RemoteCertificateValidationCallback certificateValidator = null)
+        protected override Task<IMailService> CreateIncomingMailClientAsync(CancellationToken cancellationToken = default, RemoteCertificateValidationCallback certificateValidator = null)
           => UseImapClient ? 
-            await base.CreateIncomingMailClientAsync(cancellationToken) : 
-            await new Pop3Client().ConnectAsync(Configuration, cancellationToken, certificateValidator);
+            base.CreateIncomingMailClientAsync(cancellationToken) : 
+            new Pop3Client().ConnectAsync(Configuration, cancellationToken, certificateValidator);
     }
 }
